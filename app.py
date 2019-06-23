f4a009484a6ce6c823129275f9feccf783fb6566
stevenl256 <41574034+stevenl256@users.noreply.github.com>
2019-06-23 11:35:20 -0400

    Update ip

--- app.py
+++ app.py
@@ -89,4 +89,4 @@ def index():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app_stock.run(host='0.0.0.0', port=port)
+    app_stock.run(host='127.0.0.0', port=port)
